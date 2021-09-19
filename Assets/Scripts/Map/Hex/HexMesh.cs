495c2db35f54b78f60a3f85a1d6a1d0fe624b447
Lewis Dean <s5008913@bournemouth.ac.uk>
2021-09-19 19:37:30 +0100

    feat: calculate tris on corners with terraces and cliffs

--- Assets/Scripts/Map/Hex/HexMesh.cs
+++ Assets/Scripts/Map/Hex/HexMesh.cs
@@ -171,6 +171,27 @@ public class HexMesh : MonoBehaviour
 				);
 				return;
 			}
+			if (rightEdgeType == HexEdgeType.FLAT)
+			{
+				TriangulateCornerTerraces(
+					left, leftCell, right, rightCell, bottom, bottomCell
+				);
+				return;
+			}
+			TriangulateCornerTerracesCliff(
+				bottom, bottomCell, left, leftCell, right, rightCell
+			);
+			return;
+		}
+		if (rightEdgeType == HexEdgeType.SLOPE_TERRACE)
+		{
+			if (leftEdgeType == HexEdgeType.FLAT)
+			{
+				TriangulateCornerTerraces(
+					right, rightCell, bottom, bottomCell, left, leftCell
+				);
+				return;
+			}
 		}
 
 		AddTriangle(bottom, left, right);
@@ -207,6 +228,59 @@ public class HexMesh : MonoBehaviour
 
 		AddQuad(v3, v4, left, right);
 		AddQuadColorPerVertex(c3, c4, leftCell.color, rightCell.color);
+	}
+
+	void TriangulateCornerTerracesCliff(
+		Vector3 begin, HexCell beginCell,
+		Vector3 left, HexCell leftCell,
+		Vector3 right, HexCell rightCell
+)
+	{
+		float b = 1f / (rightCell.Elevation - beginCell.Elevation);
+		Vector3 boundary = Vector3.Lerp(begin, right, b);
+		Color boundaryColor = Color.Lerp(beginCell.color, rightCell.color, b);
+
+		TriangulateBoundaryTriangle(
+		begin, beginCell, left, leftCell, boundary, boundaryColor
+		);
+
+		if (leftCell.GetEdgeType(rightCell) == HexEdgeType.SLOPE_TERRACE)
+		{
+			TriangulateBoundaryTriangle(
+				left, leftCell, right, rightCell, boundary, boundaryColor
+			);
+		}
+		else
+		{
+			AddTriangle(left, right, boundary);
+			AddTriangleColorPerVertex(leftCell.color, rightCell.color, boundaryColor);
+		}
+	}
+
+	void TriangulateBoundaryTriangle(
+		Vector3 begin, HexCell beginCell,
+		Vector3 left, HexCell leftCell,
+		Vector3 boundary, Color boundaryColor
+	)
+	{
+		Vector3 v2 = HexMetrics.TerraceLerpBetweenPoints(begin, left, 1);
+		Color c2 = HexMetrics.TerraceLerpBetweenColors(beginCell.color, leftCell.color, 1);
+
+		AddTriangle(begin, v2, boundary);
+		AddTriangleColorPerVertex(beginCell.color, c2, boundaryColor);
+
+		for (int i = 2; i < HexMetrics.terraceSteps; i++)
+		{
+			Vector3 v1 = v2;
+			Color c1 = c2;
+			v2 = HexMetrics.TerraceLerpBetweenPoints(begin, left, i);
+			c2 = HexMetrics.TerraceLerpBetweenColors(beginCell.color, leftCell.color, i);
+			AddTriangle(v1, v2, boundary);
+			AddTriangleColorPerVertex(c1, c2, boundaryColor);
+		}
+
+		AddTriangle(v2, left, boundary);
+		AddTriangleColorPerVertex(c2, leftCell.color, boundaryColor);
 	}
 
 	void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
